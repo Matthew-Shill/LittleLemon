97091d1d13608b1d086682bba183e129860e811f
Matthew Shill <matthewshill@Matthews-MacBook-Pro.local>
2023-11-06 22:52:55 -0700

    Added APIs

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from .views import MenuItemsView, SingleMenuItemView, MenuViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'menu', MenuViewSet, basename='menu')
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu', views.MenuItemsView.as_view(), name='menu-items'),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name='single-menu-item'),
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
