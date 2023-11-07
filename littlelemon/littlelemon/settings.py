39ecdfc087d5388907b5b9311fa016db5add178f
Matthew Shill <matthewshill@Matthews-MacBook-Pro.local>
2023-11-06 20:41:55 -0700

    Commit 3 - Set up Models & MySQL

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -72,12 +72,20 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 
 # Database
+# Used user: 'mattshill' and password: 'matt123' because user: root and password: '' did not work.
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'littlelemon',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "Set sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
